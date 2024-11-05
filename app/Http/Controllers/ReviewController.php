6eb93ff6411c0269f9a79a46b4c0ba2c437c3c5e
Buithihuong2210 <huongbtgch200638@fpt.edu.vn>
2024-11-05 22:38:47 +0700

    route

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -69,6 +69,7 @@ class ReviewController extends Controller
             return response()->json(['message' => 'You have already reviewed this order.'], 403);
         }
 
+
         // Lưu đánh giá cho từng sản phẩm
         foreach ($request->product_reviews as $index => $reviewData) {
             // Tìm sản phẩm tương ứng từ order_items dựa trên thứ tự
