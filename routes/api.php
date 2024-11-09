beeea6511000c09780a870d3048019c057d356c1
Buithihuong2210 <huongbtgch200638@fpt.edu.vn>
2024-11-10 00:56:09 +0700

    fix

--- routes/api.php
+++ routes/api.php
@@ -341,6 +341,5 @@ Route::middleware(['auth:sanctum', 'role:staff'])->prefix('manager')->group(func
     });
 
     Route::post('/orders/confirm-delivery/{order_id}', [OrderController::class, 'confirmDelivery']);
-
 });
 
